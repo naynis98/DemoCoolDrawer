85099ca3853d52c018b5b5826a195520cf848a3c
Teo Sin Yan <16033774@myrp.edu.sg>
2018-08-07 11:57:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16033774/democooldrawer/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.a16033774.democooldrawer;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] drawerItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    ArrayAdapter<String> aa;
+    String currentTitle;
+    ActionBar ab;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerItems = new String[]{"Bio", "Vaccination", "Anniversary"};
+        ab = getSupportActionBar();
+
+        aa = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_activated_1, drawerItems);
+        drawerList.setAdapter(aa);
+
+        // Set the list's click listener
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int
+                    position, long arg3) {
+
+                Fragment fragment = null;
+                if (position == 0)
+                    fragment = new BioFragment();
+                else if (position == 1)
+                    fragment = new VaccinationFragment();
+                else if (position == 2)
+                    fragment = new AnniversaryFragment();
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction trans = fm.beginTransaction();
+                trans.replace(R.id.content_frame, fragment);
+                trans.commit();
+
+                // Highlight the selected item,
+                //  update the title, and close the drawer
+                drawerList.setItemChecked(position, true);
+                currentTitle = drawerItems[position];
+                ab.setTitle(currentTitle);
+                drawerLayout.closeDrawer(drawerList);
+            }
+        });
+
+
+    }
+}
